ae15ae02b414b2c1e95c0b6cb7f6a564dec8a4b3
kenhardika <kenzamahardika@gmail.com>
2022-05-20 11:13:07 +0700

    update all, add js button AI or Human

--- script.js
+++ script.js
@@ -10,8 +10,54 @@ function startGame() {
         }
         layerOffAnimation();
         setTimeout(layerOffDisplay, 500);
+        mainMenu();
     }
 }
+
+function mainMenu() {
+    const vsAIBtn = document.getElementById('vsAIBtn');
+    const vsHumanBtn = document.getElementById('vsHumanBtn');
+    vsAIBtn.onclick=()=>{ 
+        gameModes.vsAI();
+    };
+    vsHumanBtn.onclick=()=> {
+        gameModes.vsHuman();
+    };
+
+    const gameModes = (() => {
+        function vsAI() {
+            console.log('Versus AI Mode');
+        }
+        function vsHuman() {
+            console.log('Versus Human Mode');
+        }
+        return {
+            vsAI: vsAI,
+            vsHuman: vsHuman
+        }
+    }) ();
+    const boardLayout = () => {
+      const boardLay = document.querySelector('.boardGame');
+        
+      for ( let i=0; i<3; i++) {
+          for(let j=0; j<3;j++){
+            const gridBoard = document.createElement('div');
+            //const para = document.createElement('p');
+            gridBoard.className=`grid ${i}-${j}`;
+            //gridBoard.style.border="solid 1px black";
+           // gridBoard.append(para);
+            boardLay.append(gridBoard);
+            }
+        }
+    }
+
+    return {
+        gameModes: gameModes,
+        boardLayout: boardLayout
+    }
+}
+
+
 
 window.onload = () => {
 startGame();
